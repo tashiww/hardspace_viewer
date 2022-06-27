635f62844d70a5c4200fbd834744d29e880bdedf
dnleek <daniellee@pioneers.berkeley.edu>
2022-06-27 05:51:01 -0700

    Undo accidental changes from last commit
    Oops, my bad

--- dynamicUpdate.js
+++ dynamicUpdate.js
@@ -107,7 +107,7 @@ function init() {
 				row.cells[1].innerHTML = Number(sorted_scrap[i-1][1].toFixed(0)).toLocaleString();
 			}
 		}
-		else {
+		else {  // salvaged block
 			salvage_value += msg.value;
 			score = calcScore(salvage_value, timebonuses[timebonus_index], mission_bonus);
 			updateElement('total_score', score);
@@ -135,7 +135,7 @@ function init() {
 		updateBar(timestamp, salvage_value, scrap_value)
 
 		table = document.getElementById('checklist');
-		for (var i = 1; i < table.rows.length-1; i++) {
+		for (var i = 1; i < table.rows.length; i++) {
 			var row = table.rows[i]
 			var item = row.cells[0].firstChild.value
 			var target = row.cells[1].firstChild.value
